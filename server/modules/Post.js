205963cb4213ee470ddff3d086099d3d27f39f1d
Mohammad Zaid <mz188646@gmail.com>
2024-05-23 16:20:07 +0300

    create schema post

---
+++ server/modules/Post.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const postSchema = new Schema({
+      title: {
+            type: String,
+            require: true
+      },
+      body: {
+            type: String,
+            require: true
+      },
+      createdAt: {
+            type: Date,
+            default: Date.now
+      },
+      updatedAt: {
+            type: Date,
+            default: Date.now
+      }
+});
+module.exports = mongoose.model("Post", postSchema)
