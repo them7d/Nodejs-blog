e6e0a38f5b575e73f7cd21490833b698bfdd2f1b
Mohammad Zaid <mz188646@gmail.com>
2024-05-23 16:22:00 +0300

    add dotenv

--- index.js
+++ index.js
@@ -2,9 +2,12 @@ const express = require("express")
 const expressLayouts = require("express-ejs-layouts")
 const router = require("./server/routes/main");
 const app = express();
-
+const connect_db = require("./server/config/db.js")
 app.set("view engine", "ejs");
 app.use(express.static('public'))
+require("dotenv").config();
+
+connect_db();
 
 app.use(expressLayouts);
 app.set("layout", "./layouts/main")
