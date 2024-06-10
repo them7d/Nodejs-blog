1294e52da01deb3d4e3de5bb54a16e3e70d2654e
Mohammad Zaid <mz188646@gmail.com>
2024-06-11 01:02:42 +0300

    add cookie parser & session & admin panel

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const express = require("express")
 const expressLayouts = require("express-ejs-layouts")
+const cookieParser = require("cookie-parser")
+const session = require("express-session")
+const MongoStore = require("connect-mongo")
 const router = require("./server/routes/main");
 const app = express();
 const connect_db = require("./server/config/db.js")
@@ -8,14 +11,25 @@ app.use(express.static('public'))
 require("dotenv").config();
 
 connect_db();
-
+require('dotenv').config()
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(cookieParser());
+
+app.use(session({
+      secret: "keyboard cat",
+      resave: false,
+      saveUnitialized: true,
+      store: MongoStore.create({
+            mongoUrl: process.env.MONGODB_URI
+      })
+}))
 
 app.use(expressLayouts);
 app.set("layout", "./layouts/main")
 
 app.use("/", router);
+app.use("/", require("./server/routes/admin.js"));
 
 app.listen(5000, () => {
       console.log(`servcer is running on port 5000`);
